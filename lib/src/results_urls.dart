9a48b892a30a88236b77b4d0c67b72c28f5a5bf6
Edwin Magabe <edyma50@yahoo.com>
2019-12-14 02:54:56 +0300

    added a class that simplify working with urls

---
+++ lib/src/results_urls.dart
@@ -0,0 +1,49 @@
+typedef String BaseUrlFinalizer({String url, String baseUrl});
+
+String getBaseUrl(String url, {BaseUrlFinalizer baseUrlFinalizer}) {
+  String bUrl;
+  String protocolStr;
+
+  String tmpUrl = url.trim();
+  final bool t1 = tmpUrl.contains("https://");
+  final bool t2 = tmpUrl.contains("HTTPS://");
+  if (t1 || t2) {
+    protocolStr = "https://";
+    if (t1) {
+      tmpUrl = tmpUrl.replaceFirst("https://", '');
+    } else {
+      tmpUrl = tmpUrl.replaceFirst("HTTPS://", '');
+    }
+  } else {
+    protocolStr = "http://";
+    tmpUrl = tmpUrl.replaceFirst("http://", '');
+    tmpUrl = tmpUrl.replaceFirst("HTTP://", '');
+  }
+
+  var list = tmpUrl.split("/");
+  if (list.isEmpty) {
+    return '';
+  }
+  list.removeLast();
+  bUrl = list.join("/");
+  if (baseUrlFinalizer != null) {
+    return baseUrlFinalizer(url: url, baseUrl: '$protocolStr$bUrl/');
+  } else {
+    return '$protocolStr$bUrl/';
+  }
+}
+
+String getResultsPath(String url) {
+  //some paths contain \ ,so stupid ,those who built the necta site
+  var list = url.replaceAll('\\', '/').split("/");
+  if (list.isNotEmpty) {
+    return list[list.length - 1];
+  } else {
+    return '';
+  }
+}
+
+String getFullUrl(String baseUrl, String pathOrFilename) {
+  String bUrl = baseUrl;
+  return '$bUrl${getResultsPath(pathOrFilename)}';
+}
