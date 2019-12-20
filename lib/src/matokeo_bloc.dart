12b37c0f17a55df5037c9fd5409efa523636c5a0
Edwin Magabe <edyma50@yahoo.com>
2019-12-20 10:34:27 +0300

    modified

--- lib/src/matokeo_bloc.dart
+++ lib/src/matokeo_bloc.dart
@@ -4,10 +4,11 @@
  */
 
 import '../matokeo_core.dart';
+import 'package:meta/meta.dart';
+import 'dart:async';
 
-abstract class MatokeoBloc<E, S> extends MBloc<E, S> {
-  Future<void> load(String xml, {String baseUrl});
-}
+typedef void OnDone();
+typedef void DecoderListener<T>(T entity);
 
 class MatokeoBlocException implements Exception {
   final String message;
@@ -17,5 +18,67 @@ class MatokeoBlocException implements Exception {
   @override
   String toString() {
     return message;
+  }
+}
+
+abstract class MatokeoBloc<E, S> extends MBloc<E, S> {
+  Completer<void> _completer;
+  StreamSubscription _subscription;
+  Stream _stream;
+
+  Future<void> unsubscribe() async {
+    if ((_subscription != null) && (_stream != null)) {
+      try {
+        await _subscription.cancel();
+        print('----------------unsubscribe---------');
+      } catch (_) {}
+    }
+  }
+
+  Future<void> load(String xml, {String baseUrl}) async {
+    if (xml == null || xml.isEmpty) {
+      throw MatokeoBlocException('xml is null or empty');
+    }
+
+    if ((_completer != null) && (!_completer.isCompleted)) {
+      return; //avoid data corruption
+    }
+
+    _completer = Completer();
+
+    //unsubscribe previous stream
+    await unsubscribe();
+
+    try {
+      eventsDispatcher(xml, baseUrl: baseUrl);
+    } catch (e) {
+      throw MatokeoBlocException(e.toString());
+    }
+  }
+
+  void eventsDispatcher(String xml, {String baseUrl});
+
+  void decode<T>({
+    @required String xml,
+    @required ResultsXmlDecoder<T> decoder,
+    @required DecoderListener<T> listener,
+    @required OnDone onDone,
+  }) {
+    _stream = decoder.decode(xml);
+    _subscription = _stream.listen(listener, onDone: onDone);
+  }
+
+  /// A subclasss must call this when loading is completed
+  /// so to be able to reload or load other xml using this block
+  void blocCompleted() {
+    if (!_completer.isCompleted) {
+      _completer.complete();
+    }
+  }
+
+  @override
+  Future<void> dispose() async {
+    await unsubscribe();
+    return super.dispose();
   }
 }
