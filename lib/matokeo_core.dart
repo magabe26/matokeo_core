fb8fb81fdb337d9a3de1be3e8ac9705fab9a13bd
Edwin Magabe <edyma50@yahoo.com>
2020-01-02 11:04:48 +0300

    added export

--- lib/matokeo_core.dart
+++ lib/matokeo_core.dart
@@ -6,5 +6,6 @@
 library matokeo_core;
 
 export 'package:simple_smart_scraper/simple_smart_scraper.dart';
+export 'package:simple_smart_scraper/petitparser_2.4.0.dart';
 export 'src/cached_result_data.dart';
 export 'src/matokeo_bloc_exception.dart';
